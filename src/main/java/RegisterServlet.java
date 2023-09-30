0b67eb80f84d64ad1ab5b6d566dd52845af4dc62
kauroah <kauroah@gmail.com>
2023-09-30 13:15:19 +0300

    Tomcat

--- src/main/java/RegisterServlet.java
+++ src/main/java/RegisterServlet.java
@@ -4,28 +4,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-/**
- * Servlet implementation class Register
- */
 @WebServlet("/Register")
 public class RegisterServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public RegisterServlet() {
         super();
     }
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().append("Served at: ").append(request.getContextPath());
     }
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String uname=request.getParameter("uname");
         String password=request.getParameter("password");
